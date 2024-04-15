c831fd79f40263a955fdee67dcd4430a66bae2ca
Henry Yeung <henrywfyeung@gmail.com>
2024-04-15 19:21:35 +0800

    make public load option function

--- src/settings.rs
+++ src/settings.rs
@@ -30,7 +30,7 @@ pub struct Settings {
 }
 
 impl Settings {
-  pub(crate) fn load(options: Options) -> Result<Settings> {
+  pub fn load(options: Options) -> Result<Settings> {
     let mut env = BTreeMap::<String, String>::new();
 
     for (var, value) in env::vars_os() {
